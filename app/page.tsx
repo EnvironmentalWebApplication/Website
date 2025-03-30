edb06b9410d9396586e841ab90879bd232422ee6
Josh Parent <joshuamparent@gmail.com>
2025-03-29 22:26:39 -0400

    Updated navbar.  Added project overview and data pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import NavigationMenu, {
+import Link from "next/link";
+import {
+  NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuContent,
+  NavigationMenuLink,
 } from "@/components/ui/navigation-menu";
 
 export default function Home() {
@@ -11,27 +12,33 @@ export default function Home() {
       <div className="min-h-screen flex flex-col overflow-hidden">
         {/* Navigation Menu */}
         <header className="bg-background border-b border-border">
-          <NavigationMenu className="container mx-auto p-4">
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Home</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <div className="p-4">Welcome to the Home Page</div>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>About</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <div className="p-4">Here's some information about us.</div>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Contact</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <div className="p-4">Get in touch with us here.</div>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
+          <NavigationMenu className="container p-4 flex items-center">
+            {/* Logo - Left-aligned */}
+            <div className="flex items-center mr-10">
+              <img src="/RPILogo.png" alt="Logo" className="h-8 w-auto mr-4" />
+              <img src="/parkLogo.png" alt="Logo" className="h-8 w-auto" />
+            </div>
+
+            {/* Navigation Links - Centered */}
+            <div className="flex-1 flex justify-center">
+              <NavigationMenuList className="flex space-x-4">
+                <NavigationMenuItem>
+                  <NavigationMenuLink asChild>
+                    <Link href="/">Home</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuLink asChild>
+                    <Link href="/project-overview">Project Overview</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuLink asChild>
+                    <Link href="/data">Data</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </div>
           </NavigationMenu>
         </header>
 
