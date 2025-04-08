6330834d9fd0e8ee02034f577a4da9644a8d512a
Josh Parent <joshuamparent@gmail.com>
2025-04-07 21:30:03 -0400

    Deploying website

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "website";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-    devIndicators: false
+    devIndicators: false,
+    output: "export",
+    basePath: isProd ? `/${repo}` : "",
+    assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
